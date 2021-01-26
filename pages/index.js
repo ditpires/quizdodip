7d9e3a863dce557f74efa8ec81b9d367b82b9d5c
Diogo Pires <diogo.pires@alura.com.br>
2021-01-25 22:16:34 -0300

    inicio aula 03

--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,13 @@ import db from '../db.json';
 import Widget from '../src/components/Widget';
 import QuizLogo from '../src/components/QuizLogo';
 import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
 import Footer from '../src/components/Footer';
 import GitHubCorner from '../src/components/GitHubCorner';
+import Input from '../src/components/Input';
+import Button from '../src/components/Button';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-
-//const BackgroundImage = styled.div`
-//  background-image: url(${db.bg});
-//  flex: 1;
-//  background-size: cover;
-//  background-position: center;
-//`;
-
-export const QuizContainer = styled.div`
-  width: 100%;
-  max-width: 350px;
-  padding-top: 45px;
-  margin: auto 10%;
-  @media screen and (max-width: 500px) {
-    margin: auto;
-    padding: 15px;
-  }
-`;
 
 
 export default function Home() {
@@ -52,19 +37,20 @@ export default function Home() {
           <h1>{db.title}</h1>
           </Widget.Header>
           <Widget.Content>
-            <form onSubmit={function (infosDoEvento) {
+            <form onSubmit={(infosDoEvento) => {
               infosDoEvento.preventDefault();
               router.push(`/quiz?name=${name}`);
             }}>
-              <input  
-                onChange={function (infosDoEvento) {
+              <Input  
+                onChange={(infosDoEvento) => {
                  setName(infosDoEvento.target.value);
                 }}
                 placeholder="Preencha seu nome aqui"
+                value={name}
               />
-              <button type="submit" disabled={name.length === 0}>
-                Jogar {name}
-              </button>
+              <Button type="submit" disabled={name.length === 0}>
+                {`Bora jogar, ${name} ?`}
+              </Button>
             </form>
           </Widget.Content>
         </Widget>
--- pages/quiz.js
+++ pages/quiz.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import db from '../db.json';
+import Widget from '../src/components/Widget';
+import QuizLogo from '../src/components/QuizLogo';
+import QuizBackground from '../src/components/QuizBackground';
+import QuizContainer from '../src/components/QuizContainer';
+import Button from '../src/components/Button';
 
 export default function QuizPage () {
+    const question = db.questions[0]; 
+    
     return (
-        <div>
-            Página do Quiz
-        </div>
+      <QuizBackground backgroundImage={db.bg}>
+        <QuizContainer>
+          <QuizLogo />
+          <Widget>
+            <Widget.Header>
+            <h3>
+            Pergunta
+            1
+            de
+            {` ${db.questions.length} `}
+            </h3>
+            </Widget.Header>
+
+            <img 
+                alt="Descrição"
+                style={{
+                    width: '100%',
+                    height: '150px',
+                    objectFit: 'cover',
+                }}
+                src={question.image}            
+            />
+            <Widget.Content>
+                <h2>
+                    {question.title}
+                </h2>
+                <p>
+                    {question.description}
+                </p>
+                <Button>
+                  Confirmar
+                </Button>
+            </Widget.Content>
+          </Widget>
+          <Widget>
+            <Widget.Content>
+              <h1>Quizes da Galera</h1>
+              <p>lorem ipsum dolor sit amet...</p>
+            </Widget.Content>
+          </Widget>
+        </QuizContainer>
+      </QuizBackground>
     );
 }
+  
